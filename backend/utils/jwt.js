866a8c9f88d463ae0104c5a4220c749ad828859e
Nithiyanantham <110277917+Nandhan007@users.noreply.github.com>
2024-08-09 14:27:21 +0530

    Update jwt.js

--- backend/utils/jwt.js
+++ backend/utils/jwt.js
@@ -7,6 +7,7 @@ const sendToken = (user, statusCode, res) => {
     ),
     httpOnly: true,
     sameSite: "Lax",
+    secure: true,
   };
 
   res.status(statusCode).cookie("token", token, options).json({
