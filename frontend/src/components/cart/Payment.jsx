8fc37f4bc0a7de9e579b3e1445e9a4bdf8bb79d7
Nithiyanantham <110277917+Nandhan007@users.noreply.github.com>
2024-08-09 23:05:46 +0530

    Update Payment.jsx

--- frontend/src/components/cart/Payment.jsx
+++ frontend/src/components/cart/Payment.jsx
@@ -64,7 +64,7 @@ export default function Payment() {
     document.querySelector("#pay_btn").disabled = true;
 
     const data = await axios
-      .post("http://localhost:8000/api/v1/payment/process", PaymentData, {
+      .post("https://mern-wao5.onrender.com/api/v1/payment/process", PaymentData, {
         withCredentials: true,
       })
       .then((res) => res.data);
