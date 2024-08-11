20716e632f0034073e1b189b9195a95aacc643fe
Nithiyanantham <r.nandhanraghu123@gmail.com>
2024-08-11 19:19:18 +0530

    totalamount Fixed to 2 decimal

--- frontend/src/components/admin/Dashboard.jsx
+++ frontend/src/components/admin/Dashboard.jsx
@@ -45,7 +45,7 @@ export default function Dashboard() {
               <div className="card-body">
                 <div className="text-center card-font-size">
                   Total Amount
-                  <br /> <b>${totalAmount}</b>
+                  <br /> <b>${totalAmount.toFixed(2)}</b>
                 </div>
               </div>
             </div>
