1a77844f773ba6ce1f0f9e7274b130ad37a760e8
Nithiyanantham <r.nandhanraghu123@gmail.com>
2024-08-11 19:42:54 +0530

    Margin footer adjustment

--- frontend/src/components/layout/Footer.jsx
+++ frontend/src/components/layout/Footer.jsx
@@ -6,7 +6,7 @@ export default function Footer() {
   return (
     <footer className="py-1">
       <p
-        className="text-center"
+        className="text-center mt-1"
         style={{
           backgroundColor: `${isAdmin ? "#232f3e" : "#fff"}`,
           color: `${isAdmin ? "#fff" : "#000"}`,
